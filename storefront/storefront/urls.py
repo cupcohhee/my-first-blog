15a1c2a92bdae415ed518c170afb15ab20e5e7ee
cupcohhee <lyon671025@gmail.com>
2022-10-31 09:25:08 +0000

    123

--- storefront/storefront/urls.py
+++ storefront/storefront/urls.py
@@ -1,7 +1,7 @@
-"""website URL Configuration
+"""storefront URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+    https://docs.djangoproject.com/en/4.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -13,14 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from xml.etree.ElementInclude import include
 from django.contrib import admin
-from django.urls import path, include
-import debug_toolbar
+from django.urls import path
 
-# Send request to playground will lead to the url bellow
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls')),
-    path('__debug__/', include('debug_toolbar.urls'))
 ]
