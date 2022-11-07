b4f27d29daa964b94f9f1cb22db64e3453cb4ebb
cupcohhee <lyon671025@gmail.com>
2022-11-07 20:58:15 +0000

    123

--- storefront/blog/models.py
+++ storefront/blog/models.py
@@ -1,3 +1,19 @@
+from unittest.util import _MAX_LENGTH
+from django.conf import settings
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class post(models.Model):
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    author  = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    create_date = models.DateTimeField(default = timezone.now)
+    publish_date = models.DateTimeField(blank = True, null = True)
+    
+    def publish(self):
+        self.publish_date = timezone.now
+        self.save()
+    
+    def __str__(self) -> str:
+        return self.title
+         
